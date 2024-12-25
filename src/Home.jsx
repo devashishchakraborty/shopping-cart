31a984085527eb9b0eedcafdc101676737649db4
Devashish Chakraborty <devashishchakra@gmail.com>
2024-12-26 02:28:30 +0530

    Finalised the Featured section of the main page. Also added the function to add items to cart. Added styles to appropriate components as well

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,39 +1,70 @@
 import { Link } from "react-router-dom";
 import "./Home.css";
 import scrollDownIcon from "./assets/scrolldownicon.svg";
+import { useOutletContext } from "react-router-dom";
 
-const Home = () => (
-  <>
-    <div className="heroSection">
-      <div className="content">
-        <div className="welcomeMessage">
-          Welcome to <br></br> <h1>The OdinShop Store!</h1>
+const Home = () => {
+  const { products, setCart } = useOutletContext(); // Access the passed props
+  const handleAddToCart = (id) => {
+    setCart((prev) =>
+      Object.keys(prev).includes(id)
+        ? { ...prev, id: prev[id] + 1 }
+        : { ...prev, id: 1 }
+    );
+  };
+
+  return (
+    <>
+      <div className="heroSection">
+        <div className="content">
+          <div className="welcomeMessage">
+            Welcome to <br></br> <h1>The OdinShop Store!</h1>
+          </div>
+          <div className="tagline">
+            Discover the perfect blend of style and tech! Explore our curated
+            collection of electronics, stunning jewelry, men's fashion, and
+            women's clothing. Free shipping across the U.S. and quality you can
+            count on—shop now and elevate your lifestyle!
+          </div>
+          <div className="heroSectionBtns">
+            <Link to="/shop">
+              <button className="shopNowBtn">SHOP NOW</button>
+            </Link>
+            <Link to="/home" className="aboutUsBtn">
+              about us
+            </Link>
+          </div>
         </div>
-        <div className="tagline">
-          Discover the perfect blend of style and tech! 
-          Explore our curated collection of electronics, stunning jewelry, men's fashion, and women's clothing. 
-          Free shipping across the U.S. and quality you can count on—shop now and elevate your lifestyle!
-        </div>
-        <div className="heroSectionBtns">
-          <Link to="/shop"><button className="shopNowBtn">SHOP NOW</button></Link>
-          <Link to="/home" className="aboutUsBtn">about us</Link>
-        </div>
-      </div>
-      <div className="scrollDown">
+        <div className="scrollDown">
           <img src={scrollDownIcon} alt="" />
           scroll down
+        </div>
       </div>
-    </div>
-    <div className="featuredProducts">
-      <h1 className="sectionTitle">Featured</h1>
-      <div className="products">
-        <div className="product">Product 1</div>
-        <div className="product">Product 2</div>
-        <div className="product">Product 3</div>
-        <div className="product">Product 4</div>
+      <div className="featuredProducts">
+        <h1 className="sectionTitle">Featured</h1>
+        <div className="products">
+          {products &&
+            products.slice(16, 20).map((product) => (
+              <div className="product" key={product.id}>
+                <img
+                  className="image"
+                  src={product.image}
+                  alt={product.title}
+                />
+                <div className="name">{product.title}</div>
+                <h2 className="price">${product.price}</h2>
+                <button onClick={() => handleAddToCart(product.id)}>
+                  + Add to Cart
+                </button>
+              </div>
+            ))}
+        </div>
+        <Link to="/shop">
+          <button className="viewAll">View All</button>
+        </Link>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Home;
